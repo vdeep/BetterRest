a2cbc38e5e6e82211a56855a54e05c6aa2b0e655
Vishal Kanojia <vishal.deep@live.com>
2022-03-06 23:41:23 +0530

    Challenge 2 complete

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -42,7 +42,11 @@ struct ContentView: View {
                 }
 
                 Section {
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text("\($0) cup\($0 > 1 ? "s" : "")")
+                        }
+                    }
                 } header: {
                     Text("Daily coffee intake")
                         .font(.headline)
